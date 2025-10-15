d0be986d56d6bb8283e74997bb83527c1670eb91
nurik-ctrl-s <nseidahmatov@gmail.com>
2025-10-16 02:04:41 +0600

    Add files via upload

---
+++ cs wk.js
@@ -0,0 +1,27 @@
+let day = 9;
+let dayname =5;
+switch(day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;                              
+    case 3:
+        console.log("Wednesday");
+        break;      
+    case 4: 
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday,rest day");
+        break;
+    case 7:
+        console.log("Sunday, rest day");
+        break;  
+    default:
+        console.log("Invalid day number");
+}
