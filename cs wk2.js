fb1ca45e8234e30f9d2d9c8a1fb6977e3a727da7
nurik-ctrl-s <nseidahmatov@gmail.com>
2025-10-16 02:07:31 +0600

    Add files via upload

---
+++ cs wk2.js
@@ -0,0 +1,14 @@
+let color =3;
+switch(color) {
+    case 1:
+        console.log("red");
+        break;  
+    case 2:
+        console.log("blue");
+        break;
+    case 3:
+        console.log("green");
+        break;
+   default:
+        console.log("You Fail");
+}
